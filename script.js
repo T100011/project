527ee5008254ae3b48269c3a4305f3ea5ece966e
Vit <warsix@mail.ru>
2020-12-26 23:37:44 +0300

    Методы и свойства строк и чисел

--- script.js
+++ script.js
@@ -1,146 +1,29 @@
 "use strict";
 
-let num = 20;
+const str = 'teSt';
 
-function showFirstMessage(text) {
-    console.log(text);
-    console.log(num);
-}
-
-showFirstMessage("Hello world");
-console.log(num);
-
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
-
-// function calc(a, b) {
-//     return (a + b);
-//     console.log('dwed');
-// }
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
 
 
-function ret() {
-    let num = 50;
-    return num;
-}
+const fruit = 'Some fruit';
 
-const anotherNum = ret();
-console.log(anotherNum);
+console.log(fruit.indexOf('q'));
 
-const logger = function() {
-    console.log('Hello');
-};
+const logg = 'Hello world';
 
-logger();
+console.log(logg.slice(6, 11));
 
-const calc = (a, b) => {
-    console.log('1');
-    return a + b;
-};
+console.log(logg.substring(6, 11));
 
-let val = 7;
-function createAdder() {
-    function addNumbers(a, b) {
-        let ret = a + b;
-        return ret;
-    }
-    return addNumbers;
-}
-let adder = createAdder();
-let sum = adder(val, 8);
-console.log('example of function returning a function', sum);
+console.log(logg.substr(6, 5));
 
-// замыкания
-function createCounter() {
-    let counter = 0;
-    const myFunction = function() {
-        counter = counter + 1;
-        return counter;
-    };
-    return myFunction;
-}
-const increment = createCounter();
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
-console.log('example increment', c1, c2, c3);
+// числа
+const num = 12.2;
+console.log(Math.round(num));
 
-let c = 4;
-const addX = x => n => n + x;
-const addThree = addX (3);
-let d = addThree(c);
-console.log('exzmple partial application', d);
-
-let c = 4;
-function addX(x) {
-    return function(n) {
-        return n + x;
-    };
-}
-const addThree = addX (3);
-let d = addThree(c);
-console.log('exzmple partial application', d);
-
-// замыкания изнутри
-let name = 'John';
-function sayHi() {
-    console.log('Hi, ' + name);
-}
-name = 'Pete';
-sayHi();
-
-function makeWorker() {
-    let name ='Pete';
-    return function() {
-        console.log(name);
-    };
-}
-let name = 'John';
-let work = makeWorker();
-work();
-
-function sayHiBye(firstName, lastName) {
-    function getFullname() {
-        return firstName + " " + lastName;
-    }
-    console.log('Hello, ' + getFullname());
-    console.log('Bye, ' + getFullname());
-}
-
-function User(name) {
-    this.sayHi = function() {
-        console.log(name);
-    };
-}
-let user = new User('John');
-user.sayHi();
-
-function makeCounter() {
-    let count = 0;
-
-    return function() {
-        return count++;
-    };
-}
-let counter = makeCounter();
-console.log(counter());
-console.log(counter());
-console.log(counter());
-
-function makeCounter() {
-    let count = 0;
-
-    return function() {
-        return count++;
-    };
-}
-
-let counter1 = makeCounter();
-let counter2 = makeCounter();
-
-console.log(counter1());
-console.log(counter1());
-console.log(counter2());
-
+const test = '12.2px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
