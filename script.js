fba2f8665d5e63931626c8fdf74556d63a588a65
Vit <warsix@mail.ru>
2020-12-26 11:31:51 +0300

    замыкания

--- script.js
+++ script.js
@@ -39,3 +39,46 @@ const calc = (a, b) => {
     console.log('1');
     return a + b;
 };
+
+let val = 7;
+function createAdder() {
+    function addNumbers(a, b) {
+        let ret = a + b;
+        return ret;
+    }
+    return addNumbers;
+}
+let adder = createAdder();
+let sum = adder(val, 8);
+console.log('example of function returning a function', sum);
+
+// замыкания
+function createCounter() {
+    let counter = 0;
+    const myFunction = function() {
+        counter = counter + 1;
+        return counter;
+    };
+    return myFunction;
+}
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+console.log('example increment', c1, c2, c3);
+
+let c = 4;
+const addX = x => n => n + x;
+const addThree = addX (3);
+let d = addThree(c);
+console.log('exzmple partial application', d);
+
+let c = 4;
+function addX(x) {
+    return function(n) {
+        return n + x;
+    };
+}
+const addThree = addX (3);
+let d = addThree(c);
+console.log('exzmple partial application', d);
