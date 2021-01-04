78893c02f8321539cac68b543b811fecb7cfe3e4
Vit <warsix@mail.ru>
2021-01-04 23:07:55 +0300

    Передача данных по ссылке и по значению spread

--- script.js
+++ script.js
@@ -1,54 +1,125 @@
 'use strict';
-// Массивы и псевдомассивы т.е.структура которая содержит элементы по порядку
+// Передача данных по ссылке и по значению
 
-const arr = [2, 13, 26, 8, 10];
-// осортировка чисел идет как строки т.к используется алгоритм быстрой сортировки
-arr.sort(compareNum);
 
-// arr[99] = 0;
-// console.log(arr.length);
-console.log(arr);
-// функция сравнения чисел 
-function compareNum(a, b) {
-    return a - b;
+// let a = 5,
+//     b = a;
+
+//  b = b + 5;
+
+//  console.log(b);
+//  console.log(a);
+
+//  const obj = {
+//      a: 5,
+//      b: 1
+//  };
+
+//  const copy = obj;
+
+//  ссылка copy.a = 10; на объект выше const obj = {}; передача по ссылке
+//  copy.a = 10;
+
+//  console.log(copy);
+//  console.log(obj);
+
+//  делаем копию с помощью цикла 1 способ
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
 }
 
-// метод array.forEach с колбек функцией применяется к каждому элементу массива
-// item сам элемент, i номер по порядку, arr ссылка на массив который перебираем 
-// arr.forEach(function(item, i, arr ) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`)
-// });
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// делаем копию с помощью метода 2 способ
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+// соединяем объект numbers с объектом add (Object.assign-метод(numbers-объект в
+// который помещаем , add-объект который помещают)) 
+
+// console.log(Object.assign(numbers, add));
+
+// таким образом мы сделали независимую вложенную копию объекта независящую от numbers и add 
+
+// делаем копию объекта при помощи {} 
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// создаем копию массива 
+
+const oldArray = ['a', 'b', 'c'];
 
 
-// метод pop удаляет элемент из конца массива
+// const newArray = oldArray; ссылка на массив 
+// метод slice() позволяет просто скопировать старый массив 
+const newArray = oldArray.slice();
 
-// arr.pop();
+// в newArray меняем значение второго элемента 
+newArray[1] = 'dsfsdfsdf';
+console.log(newArray);
+console.log(oldArray);
 
-// метод push добавляет элемент в конец массива 
+// 4 способ создания поверхностой копии ES-6 (появился для массивом) ES 8 (для объектов)
+// оператор spread оператор разворота
 
-// arr.push(10);
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress','Livejournal', 'blogger'],
+    //   internet= [];
+// сделаем в переменной internet совмещение данных из video, blogs ид другие данные ;
+    internet= [...video, ...blogs, 'vk', 'facebook'];    
 
-// console.log(arr);
+console.log(internet);
 
-// перебираем массив через цикл for 
+// другой пример посложнее 
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+const num = [2, 5, 7];
 
-// перебираем массив через цикл for of
+log(...num);
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+// 4 способ для объектов 
 
-// // метод split(s) превращает строку в массив s разделитель 
-// const str = prompt('','');
-// const products = str.split(', ');
-// // метод sort(s) метод сортировки по алфавиту или цифрам как строки
-// products.sort();
-// // метод join(s) превращает строку в массив s разделитель 
-// console.log(products.join('; '));
+const array = ['a', 'b'];
 
-// псевдомассивы в отличии от обычных массивов не используют методы   
+const newAarray  = [...array];
 
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
