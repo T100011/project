5748475ba99e3189dcb379f553cdaab9e31a5c71
Vit <warsix@mail.ru>
2021-01-06 20:08:34 +0300

    Основы ООП, прототипно-ориентированное наследование

--- script.js
+++ script.js
@@ -1,125 +1,40 @@
 'use strict';
-// Передача данных по ссылке и по значению
+// Основы ООП, прототипно-ориентированное наследование 
 
+let str = 'some';
+let strObj = new String(str);
+// метод превращает строку в объект 
 
-// let a = 5,
-//     b = a;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-//  b = b + 5;
+console.dir([1, 2, 3]);
 
-//  console.log(b);
-//  console.log(a);
-
-//  const obj = {
-//      a: 5,
-//      b: 1
-//  };
-
-//  const copy = obj;
-
-//  ссылка copy.a = 10; на объект выше const obj = {}; передача по ссылке
-//  copy.a = 10;
-
-//  console.log(copy);
-//  console.log(obj);
-
-//  делаем копию с помощью цикла 1 способ
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
+// soldier -это прототип 
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }    
 };
 
-const newNumbers = copy(numbers);
+// мы создаем новый объект john, который будет прототипно наследоваться от john 
+const john = Object.create(soldier);
 
-newNumbers.a = 10;
-newNumbers.c.x = 10;
+// const john = {
+//     health: 100
+// };
 
-// console.log(newNumbers);
-// console.log(numbers);
+// john.__proto__ = soldier; доступ к свойствам прототипа старый способ
+// john.__proto__ = soldier;
 
-// делаем копию с помощью метода 2 способ
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-// соединяем объект numbers с объектом add (Object.assign-метод(numbers-объект в
-// который помещаем , add-объект который помещают)) 
-
-// console.log(Object.assign(numbers, add));
-
-// таким образом мы сделали независимую вложенную копию объекта независящую от numbers и add 
-
-// делаем копию объекта при помощи {} 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-// создаем копию массива 
-
-const oldArray = ['a', 'b', 'c'];
+// новый способ доступа к прототипам 
+// (1-объект которому назначается прототип, 2-сам прототип)
+// сначала создавался john, а потом soldier  
+// Object.setPrototypeOf(john, soldier);
 
 
-// const newArray = oldArray; ссылка на массив 
-// метод slice() позволяет просто скопировать старый массив 
-const newArray = oldArray.slice();
 
-// в newArray меняем значение второго элемента 
-newArray[1] = 'dsfsdfsdf';
-console.log(newArray);
-console.log(oldArray);
-
-// 4 способ создания поверхностой копии ES-6 (появился для массивом) ES 8 (для объектов)
-// оператор spread оператор разворота
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress','Livejournal', 'blogger'],
-    //   internet= [];
-// сделаем в переменной internet совмещение данных из video, blogs ид другие данные ;
-    internet= [...video, ...blogs, 'vk', 'facebook'];    
-
-console.log(internet);
-
-// другой пример посложнее 
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-// 4 способ для объектов 
-
-const array = ['a', 'b'];
-
-const newAarray  = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
+// console.log(john.armor);
+john.sayHello();
