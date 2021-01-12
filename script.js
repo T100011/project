08824ad62ea5bdb1926edaeee6c11e0b542aa5b9
Petr <warsix@mail.ru>
2021-01-13 00:03:00 +0300

    навигация по дом дереву начало

--- script.js
+++ script.js
@@ -1,60 +1,13 @@
-// 1 способ кликаем по кнопке пишем в самом html onclick="alert('Click')" не используется в современной верстке
-// 2 способ кликаем по кнопке в реальных проектах не используется
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+// console.log(document.body);
+// console.log(document.documentElement);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// позволяет найти все ноды узлы в родителе 
+console.log(document.body.childNodes);
 
-// btn.onclick = function() {
-//     alert('Second click');
-// };
+// получение первого и последнего ребенка родителя 
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-// 3 способ наиболее правильный addEventListener() добавляем обработчик события 1 аргумент назавание события click 2 аргумент callback функция)
-// callback функции выполняются строго за другими (можем создавать 
-// несколько действий на одно событие) 
-
-// btn.addEventListener('click',  () => {
-//     alert('Click');
-// });
-
-// btn.addEventListener('click',  () => {
-//     alert('Second click');
-// });
-// делаем другое действие mouseente
-
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.target);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
-
-// btn.addEventListener('click', (e) => {
-//     // смотрим на объект event 
-//     // console.log(e.target);
-//     e.target.remove();
-//     // console.log('Hover');
-// });
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-    // добавив {once: true} мы можем нажать кнопку только один раз 
-    btn.addEventListener('click', deleteElement, {once: true});
-}); 
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', function(event) {
-    // отменяем стандартное поведение 
-    event.preventDefault();
-
-    console.log(event.target);
-});
+// перемещаемся выше по к родителю от селектора с помощью parentNode
+console.log(document.querySelector('#current').parentNode.parentNode);
 
